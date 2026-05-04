09064b0462a1c70cca3722191a06f69c34e97fae
Ayman Alhawari <ayman.alhawari@gnteq.com>
2026-05-04 11:58:16 +0300

    fixes

--- FootballAI/src/FootballAI.Application/Services/AnalysisNotifier.cs
+++
@@ -1,3 +0,0 @@
-// AnalysisNotifier implementation lives in FootbalaAI.API/Services/AnalysisNotifier.cs
-// It depends on AnalysisHub (API layer), so it cannot reside in the Application layer.
-namespace FootballAI.Application.Services;
